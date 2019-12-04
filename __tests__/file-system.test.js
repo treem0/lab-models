0ecc1bfe4353d678763d7b81e176639a0cf80a06
Travis Molter <travismolter@gmail.com>
2019-12-04 15:53:44 -0800

    readDirectoryJSON function done and tested

--- __tests__/file-system.test.js
+++ __tests__/file-system.test.js
@@ -1,18 +1,22 @@
 const fs = require('fs').promises;
-const { mkdirp, writeJSON, readJSON } = require('../lib/file-systems');
+const { mkdirp, writeJSON, readJSON, readDirectoryJSON } = require('../lib/file-systems');
 
 jest.mock('fs', () => ({
   promises: {
     mkdir: jest.fn(() => Promise.resolve('My File Path')),
     writeFile: jest.fn(() => Promise.resolve()),
-    readFile: jest.fn(()=> Promise.resolve(JSON.stringify({ name: 'travis' })))
+    readFile: jest.fn(()=> Promise.resolve(JSON.stringify({ name: 'travis' }))),
+    readdir: jest.fn(() => Promise.resolve([
+      './name',
+      './secondname'
+    ])),
   }
 }));
 describe('mkdirp', () => {
   it('make a directory and all parent directories', () => {
     return mkdirp('/this/is/my/cool/spot')
       .then(()=> {
-        expect(fs.mkdir).toHaveBeenCalledWith('/this/is/my/cool/spot', {
+        expect(fs.mkdir).toHaveBeenLastCalledWith('/this/is/my/cool/spot', {
           recursive: true
         });
           
@@ -23,7 +27,7 @@ describe('writeJSON', () =>{
   it('write an object to a file', () => {
     return writeJSON('/this/is/my/cool/spot', { name: 'travis' })
       .then(() => {
-        expect(fs.writeFile).toHaveBeenCalledWith('/this/is/my/cool/spot', '{"name":"travis"}');
+        expect(fs.writeFile).toHaveBeenLastCalledWith('/this/is/my/cool/spot', '{"name":"travis"}');
       });
   });
 });
@@ -31,8 +35,20 @@ describe('readJSON', () => {
   it('read a JSON object from a file', () => {
     return readJSON('/this/is/my/cool/spot')
       .then((travis) => {
-        expect(fs.readFile).toHaveBeenCalledWith('/this/is/my/cool/spot', 'utf8');
+        expect(fs.readFile).toHaveBeenLastCalledWith('/this/is/my/cool/spot', 'utf8');
         expect(travis).toEqual({ name: 'travis' });
+      });
+  });
+});
+describe('readDirectoryJSON', () => {
+  it('read all files in a directory as objects', () => {
+    return readDirectoryJSON('./this/is/my/cool/spot')
+      .then(allItems => {
+        expect(fs.readdir).toHaveBeenLastCalledWith('./this/is/my/cool/spot');
+        expect(allItems).toEqual([
+          { name: 'travis' },
+          { name: 'travis' }
+        ]);
       });
   });
 });
